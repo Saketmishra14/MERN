bb40b0ce044ba9e91fcaac452e743e4047416766
unknown <mishrasaket042@gmail.com>
2025-09-15 10:33:01 +0530

    add style and header,footer

--- Project-1/app.js
+++ Project-1/app.js
@@ -2,12 +2,17 @@ const express=require("express")
 const app=express()
 const Listing=require("./Models/listing.js")
 const path=require("path")
+const methodOverride=require("method-override")
+const ejsMate=require("ejs-mate")
 
 const mongoose=require("mongoose")
 
 app.set("view engine","ejs")
 app.set("views",path.join(__dirname,"views"))
 app.use(express.urlencoded({extended:true}))
+app.use(methodOverride("_method"))
+app.engine("ejs",ejsMate)
+app.use(express.static(path.join(__dirname,"/public")));
 
 main()
 .then(()=>console.log("Database is connected."))
@@ -40,6 +45,24 @@ app.post("/listings",async(req,res)=>{
     // const {title,description,image,location,country,price}=req.body;
     const newListing=new Listing(req.body.listing)
     await newListing.save()
+    res.redirect("/listings")
+})
+//Edit Route
+app.get("/listings/:id/edit",async(req,res)=>{
+    const {id}=req.params
+    const listing=await Listing.findById(id)
+    res.render("listings/edit.ejs",{listing})
+})
+//update Route
+app.put("/listings/:id",async(req,res)=>{
+    const {id}=req.params
+     await  Listing.findByIdAndUpdate(id,{...req.body.listing},{new:true})
+    res.redirect(`/listings/${id}`)
+})
+//Delete Route
+app.delete("/listings/:id",async(req,res)=>{
+    const {id}=req.params
+    await Listing.findByIdAndDelete(id)
     res.redirect("/listings")
 })
 
